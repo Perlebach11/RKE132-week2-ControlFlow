980ab4373a2bc995d08ef6f91336a9707d74750a
Perlebach <markenkeres@gmail.com>
2024-04-26 17:01:36 +0300

    add USERFGENDERVALIDATION code

---
+++ UserAgeValidation2/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//rakendus küsib kasutajal sisestada tema vanuse
+//kui kasutaja vanus on väiksem kui 13, siis konsoolis kuvatakse:
+//"you are too young to use instagram"
+//muul juhul
+//konsoolis kuvatakse "Welcome to instagram"
+
+Console.WriteLine("enter your age:");
+//int userAge = Int32. Parse (Console.ReadLine()); // kui see oleks sõne ehk "13" -heap, 13- stack
+
+string userAge = Console.ReadLine();
+int userAgeNum = 0;
+
+//boolean- true/false
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+
+Console.WriteLine($"Parse result {isAgeNumber}. User is {userAgeNum} years old");
+
+if (isAgeNumber ==true)
+{
+    if (userAgeNum >= 13)
+
+    {
+        Console.WriteLine("Welcome to instagram!");
+    }
+
+    else {
+        Console.WriteLine("you are too young to use instagram");
+    }
+} 
+else
+{
+    Console.WriteLine("Could not read your age.");
+}
